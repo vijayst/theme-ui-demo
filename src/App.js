184637a36cbf5c5efbb90ad199ae7a2a3c43e22c
Vijay T <vijayst@gmail.com>
2021-08-18 10:09:34 +0530

    install theme-ui

--- src/App.js
+++ src/App.js
@@ -1,20 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+/** @jsxImportSource theme-ui */
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div>
+      <header sx={{ color: 'primary' }}>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+        <a href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
           Learn React
         </a>
       </header>
