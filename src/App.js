6865b92bbdfa4f33880f94e65a1ac7f82c88aeff
Vijay T <vijayst@gmail.com>
2021-08-19 19:09:49 +0530

    basic navbar with title

--- src/App.js
+++ src/App.js
@@ -3,8 +3,26 @@
 function App() {
   return (
     <div>
-      <h1 sx={{ fontFamily: 'heading' }}>Learn React</h1>
-      <p sx={{ fontFamily: 'body' }}>React is awesome!</p>
+      <header
+        sx={{
+          backgroundColor: 'highlight',
+          height: 64,
+          display: 'flex',
+          justifyContent: 'space-between',
+          alignItems: 'center',
+        }}
+      >
+        <div
+          sx={{
+            fontFamily: 'heading',
+            fontSize: 3,
+            color: 'accent',
+            pl: 4,
+          }}
+        >
+          Exotic Expeditions
+        </div>
+      </header>
     </div>
   );
 }
--- src/theme.js
+++ src/theme.js
@@ -3,10 +3,19 @@ const theme = {
     body: 'Montserrat',
     heading: 'Raleway',
   },
+  fontSizes: [12, 14, 16, 20, 24, 32, 48],
+  lineHeights: {
+    body: 1.5,
+    heading: 1.125,
+  },
+  space: [0, 4, 8, 16, 32, 64, 128, 256, 512],
   colors: {
     text: '#000',
     background: '#fff',
-    primary: '#33e',
+    primary: '#0552ba',
+    secondary: '#fa8072',
+    accent: '#ffdab9',
+    highlight: '#002366',
   },
 };
 
